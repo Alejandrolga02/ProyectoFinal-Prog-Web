a0f1df4c0bef0f5c8bd7f42afbee196beba27aba
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2022-11-08 01:09:31 -0700

    Update administracion productos

--- js/admin-productos.js
+++ js/admin-productos.js
@@ -1,24 +1,10 @@
 "use strict";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import { getDatabase, onValue, ref, set, child, get, update, remove } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
-import {
-	getStorage,
-	deleteObject,
-	ref as refStorage,
-	uploadBytes,
-	getDownloadURL,
-} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+import { getDatabase, onValue, ref, set, child, get, update } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import { getStorage, ref as refStorage, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 
-const firebaseConfig = {
-	apiKey: "AIzaSyDDG__NXwuBiuiJimbogM-FJOQVl2nJhlc",
-	authDomain: "alejandrolga-webfinal.firebaseapp.com",
-	projectId: "alejandrolga-webfinal",
-	storageBucket: "alejandrolga-webfinal.appspot.com",
-	messagingSenderId: "527030299000",
-	appId: "1:527030299000:web:a1c1c54973bf2a9211008d",
-	measurementId: "G-13ZX3GTW9G",
-};
+import { firebaseConfig } from "./config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -40,6 +26,7 @@ const getInputs = () => {
 	event.preventDefault();
 
 	return {
+		codigo: document.querySelector("#codigo").value.trim(),
 		nombre: document.querySelector("#nombre").value.trim(),
 		precio: document.querySelector("#precio").value.trim(),
 		descripcion: document.querySelector("#descripcion").value.trim(),
@@ -48,19 +35,23 @@ const getInputs = () => {
 
 const clearInputs = () => {
 	event.preventDefault();
+	document.querySelector("#codigo").value = "";
 	document.querySelector("#nombre").value = "";
 	document.querySelector("#precio").value = "";
 	document.querySelector("#descripcion").value = "";
+	document.querySelector("#url").value = "";
 	document.querySelector("#imgPreview").classList.add("d-none");
 	results.classList.add("d-none");
 	results.innerHTML = "";
 	imagen.value = "";
 };
 
-const fillInputs = ({ nombre, descripcion, precio, url }) => {
+const fillInputs = ({ codigo, nombre, descripcion, precio, url }) => {
+	document.querySelector("#codigo").value = codigo;
 	document.querySelector("#nombre").value = nombre;
 	document.querySelector("#descripcion").value = descripcion;
 	document.querySelector("#precio").value = precio.slice(1);
+	document.querySelector("#url").value = url;
 	document.querySelector("#imgPreview").src = url;
 	document.querySelector("#imgPreview").classList.remove("d-none");
 };
@@ -72,9 +63,7 @@ const imagenChange = () => {
 
 const showAlert = (message, title) => {
 	const modalToggle = document.getElementById("alertModal");
-	const myModal = new bootstrap.Modal("#alertModal", {
-		keyboard: true,
-	});
+	const myModal = new bootstrap.Modal("#alertModal", { keyboard: true });
 
 	document.getElementById("alertTitle").innerHTML = title;
 	document.getElementById("alertMessage").innerHTML = message;
@@ -83,54 +72,54 @@ const showAlert = (message, title) => {
 };
 
 async function insertData() {
-	event.preventDefault();
-
 	try {
-		let { nombre, descripcion, precio } = getInputs();
+		event.preventDefault();
+
+		let { codigo, nombre, descripcion, precio } = getInputs();
 		const dbref = ref(db);
-		const storageRef = refStorage(storage, "productos/" + nombre);
+		const storageRef = refStorage(storage, "productos/" + codigo);
 
 		if (isNaN(parseFloat(precio)) || parseFloat(precio) <= 0) return showAlert("Introduzca un precio valido", "Error");
-		if (!nombre || !descripcion || !imagen.value) return showAlert("Existen campos vacios", "Error");
+		if (!codigo || !nombre || !descripcion || !imagen.value) return showAlert("Existen campos vacios", "Error");
 
-		const snapshot = await get(child(dbref, "productos/" + nombre));
-		if (snapshot.exists()) return showAlert("Ya existe un producto con ese nombre", "Error");
+		const snapshot = await get(child(dbref, "productos/" + codigo));
+		if (snapshot.exists()) return showAlert("Ya existe un producto con ese código", "Error");
 
 		await uploadBytes(storageRef, imagen.files[0]);
 		let url = await getDownloadURL(storageRef);
 
-		await set(ref(db, "productos/" + nombre), { descripcion, precio: "$" + precio, url });
+		await set(ref(db, "productos/" + codigo), { nombre, descripcion, precio: "$" + precio, url, status: "0" });
 
 		showAlert("Se insertaron con exito los datos", "Resultado");
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
 async function showData() {
-	event.preventDefault();
-
 	try {
-		let { nombre } = getInputs();
-		if (nombre === "") return showAlert("Introduzca un nombre", "Error");
+		event.preventDefault();
+
+		let { codigo } = getInputs();
+		if (codigo === "") return showAlert("Introduzca un código", "Error");
 
 		const dbref = ref(db);
-		const snapshot = await get(child(dbref, "productos/" + nombre));
+		const snapshot = await get(child(dbref, "productos/" + codigo));
 
 		if (snapshot.exists()) {
+			let nombre = snapshot.val().nombre;
 			let descripcion = snapshot.val().descripcion;
 			let precio = snapshot.val().precio;
 			let url = snapshot.val().url;
-			if (!url) {
-				const storageRef = refStorage(storage, "imagenVacia.svg");
-				url = await getDownloadURL(storageRef);
-			}
-			fillInputs({ nombre, descripcion, precio, url });
+
+			if (!url) url = await getDownloadURL(refStorage(storage, "imagenVacia.svg"));
+
+			fillInputs({ codigo, nombre, descripcion, precio, url });
 		} else {
 			showAlert("No se encontró el registro", "Error");
 		}
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
@@ -144,9 +133,10 @@ async function showProducts() {
 		let urlDefault = await getDownloadURL(storageRef);
 
 		await onValue(dbref, snapshot => {
-			results.innerHTML = `<thead><tr>
+			results.innerHTML = `<caption>Lista de productos</caption><thead><tr>
+					<th scope="col" width="5%" class="text-center">Código</th>
 					<th scope="col" width="30%" class="text-center">Nombre</th>
-					<th scope="col" width="40%" class="text-center">Descripcion</th>
+					<th scope="col" width="35%" class="text-center">Descripción</th>
 					<th scope="col" width="15%" class="text-center">Precio</th>
 					<th scope="col" width="15%" class="text-center">Imagen</th>
 				</tr></thead><tbody></tbody>`;
@@ -154,72 +144,67 @@ async function showProducts() {
 			snapshot.forEach(childSnapshot => {
 				const childKey = childSnapshot.key;
 				const childData = childSnapshot.val();
-				let imgURL;
-				if (childData.url === undefined) {
-					imgURL = urlDefault;
-				} else {
-					imgURL = childData.url;
+
+				if (childData.status === "0") {
+					let imgURL = childData.url;
+
+					if (childData.url === undefined) imgURL = urlDefault;
+
+					results.lastElementChild.innerHTML += `<tr>
+						<th class="text-center" scope="row">${childKey}</th>
+						<td class="text-center">${childData.nombre}</td>
+						<td class="text-center">${childData.descripcion}</td>
+						<td class="text-center">${childData.precio}</td>
+						<td class="text-center p-0"><img class="w-100" src="${imgURL}" alt="Imagen de ${childData.nombre}"/></td>
+					</tr>`;
 				}
-				results.lastElementChild.innerHTML += `<tr>
-					<th class="text-center" scope="row">${childKey}</th>
-					<td class="text-center">${childData.descripcion}</td>
-					<td class="text-center">${childData.precio}</td>
-					<td class="text-center p-0"><img class="w-100" src="${imgURL}" alt="Imagen de ${childKey}"/></td>
-				</tr>`;
 			});
 		});
 
 		results.classList.remove("d-none");
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
 async function updateData() {
-	event.preventDefault();
-
 	try {
-		let { nombre, descripcion, precio } = getInputs();
-		const storageRef = refStorage(storage, "productos/" + nombre);
+		event.preventDefault();
+
+		let { codigo, nombre, descripcion, precio } = getInputs();
+		const storageRef = refStorage(storage, "productos/" + codigo);
 
 		if (isNaN(parseFloat(precio)) || parseFloat(precio) <= 0) return showAlert("Introduzca un precio valido", "Error");
-		if (!nombre || !descripcion) return showAlert("Existen campos vacios", "Error");
+		if (!codigo || !nombre || !descripcion) return showAlert("Existen campos vacios", "Error");
 
 		if (!imagen.value) {
-			showAlert("Se realizó una actualización", "Resultado");
-			return await update(ref(db, "productos/" + nombre), { descripcion, precio: "$" + precio });
+			await update(ref(db, "productos/" + codigo), { nombre, descripcion, precio: "$" + precio, status: "0" });
+			return showAlert("Se realizó una actualización", "Resultado");
 		}
 
 		await uploadBytes(storageRef, imagen.files[0]);
 		let url = await getDownloadURL(storageRef);
-		await update(ref(db, "productos/" + nombre), { descripcion, precio: "$" + precio, url });
-		showAlert("Se realizó una actualización", "Resultado");
+
+		await update(ref(db, "productos/" + codigo), { nombre, descripcion, precio: "$" + precio, url, status: "0" });
+		return showAlert("Se realizó una actualización", "Resultado");
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
 async function deleteData() {
-	event.preventDefault();
-
 	try {
-		let { nombre } = getInputs();
-		if (nombre === "") return showAlert("Introduzca un nombre", "Error");
+		event.preventDefault();
 
-		try {
-			// Create a reference to the file to delete
-			const imageDelete = refStorage(storage, "productos/" + nombre);
-			// Delete the file
-			await deleteObject(imageDelete);
-		} catch (error) {
-			console.log(error);
-		}
+		let { codigo } = getInputs();
+		if (codigo === "") return showAlert("Introduzca un código", "Error");
 
-		await remove(ref(db, "productos/" + nombre));
-		showAlert("Se eliminó un registro", "Error");
+		await update(ref(db, "productos/" + codigo), { status: "1" });
+		showAlert("El registro fue deshabilitado", "Resultado");
+
 		await showProducts();
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
